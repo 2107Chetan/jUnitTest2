9a8b34173d6995d67be8e8f67384aba92fa4b748
Admin <Admin@10.12.18.163>
2022-11-17 23:31:19 +0530

    hello lab10

---
+++ jUnitTest/AllTests.java
@@ -0,0 +1,19 @@
+package jUnitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testAddNumbers.class, testAddStrings.class })
+public class AllTests {
+
+}
+
+
+
+
+
+
+
+
